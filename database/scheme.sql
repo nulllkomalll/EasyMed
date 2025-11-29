bc2a611b922cb2bc8617f9df2879e28c2ddf8789
nulllkomalll <nulllkomalll@gmail.com>
2025-11-29 21:59:32 +0530

    Updated landing page UI

--- database/scheme.sql
+++ database/scheme.sql
@@ -27,3 +27,20 @@ CREATE TABLE appointments (
   FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
   FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id)
 );
+
+-- Insert Patients
+INSERT INTO patients (name, email, phone, password_hash) VALUES
+('Komal ', 'komal@example.com', '9876543210', 'dummyhash123'),
+('Rohit Sharma', 'rohit@example.com', '9123456780', 'dummyhash456');
+
+-- Insert Doctors
+INSERT INTO doctors (name, email, specialty, password_hash) VALUES
+('Dr. Aditi Patil', 'aditi.derma@example.com', 'Dermatologist', 'dummyhash789'),
+('Dr. Rahul Deshmukh', 'rahul.cardio@example.com', 'Cardiologist', 'dummyhash987');
+
+-- Insert Appointments
+INSERT INTO appointments (patient_id, doctor_id, appointment_date, appointment_time, status) VALUES
+(1, 1, '2025-12-01', '10:30:00', 'Booked'),
+(2, 2, '2025-12-02', '14:00:00', 'Booked');patientspatientsappointments
+
+
